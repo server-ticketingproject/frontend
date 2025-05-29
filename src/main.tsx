d2bb4f57bbf00181f926181cdeff9c363c266361
chanhyuk05 <24sunrin106@sunrint.hs.kr>
2025-05-29 15:06:05 +0900

    setting folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './Login.tsx'
+import Login from './components/Login.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
