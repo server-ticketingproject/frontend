5c1a7dd4893b19e8e216bd512ca8b54b2fc0e5c9
officeshinyujun <officeshinyujun@gmail.com>
2025-06-10 09:06:52 +0900

    merge

---
+++ src/components/MainPage/RecommendPerform/layout.tsx
@@ -0,0 +1,115 @@
+import { VStack } from "../../VStack";
+import { SPACING } from "../../../styles/spacing";
+import { FONTS } from "../../../styles/fonts";
+import COLORS from "../../../styles/colors";
+import { HStack } from "../../HStack";
+import RecommendPerformCard from "./card";
+import Button from "../../Button";
+export default function RecommendPerformLayout() {
+
+    const recommendPerformList = [
+        {   
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },
+        {
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },
+        {
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },
+        {
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },
+        {
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },   
+        {
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },
+        {
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },
+        {
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },
+        {
+            title : "공연 이름",
+            when : "공연 일시",
+            slogan : "공연 설명",
+        },
+    ];
+
+
+
+    return (
+        <VStack
+            align="flex-start"
+            justify="flex-start"
+            gap={SPACING.medium}
+            style={{
+                width : "100%",
+                padding : `0 ${SPACING.medium}px`,
+            }}
+        >
+            <p
+                style={{
+                    fontSize : FONTS.size.head,
+                    fontWeight : FONTS.weight.w5,
+                    color : COLORS.textPrimary,
+                }}
+            >
+                요즘 인기있는 공연들
+            </p>
+            <HStack
+                align="center"
+                justify="flex-start"
+                gap={SPACING.small}
+                style={{
+                    width : "100%",
+                    overflowX : "scroll",
+                    scrollbarWidth : "none",
+                }}
+            >
+                {recommendPerformList.map((perform, index) => (
+                    <RecommendPerformCard
+                        key={index}
+                        title={perform.title}
+                        when={perform.when}
+                        slogan={perform.slogan}
+                    />
+                ))}
+            </HStack>
+            <HStack
+                align="center"
+                justify="center"
+                style={{
+                    width : "100%",
+                }}
+            >
+                <Button
+                    onClick={() => {console.log('더 많은 공연 보러가기') }}
+                    text="더 많은 공연 보러가기"
+                    fontSize={FONTS.size.body}
+                    paddingHorizontal={SPACING.huge}
+                    paddingVertical={SPACING.medium}
+                />
+            </HStack>
+        </VStack>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App.tsx';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
-    </GoogleOAuthProvider>
-  </StrictMode>
-)
+import router from './router.tsx';
+const App = () => (
+    <RouterProvider router={router}></RouterProvider>
+);
 
+createRoot(document.getElementById('root')!).render(<App />);
+export default App;
